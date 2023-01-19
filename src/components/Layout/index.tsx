b22f442ef80551437e1334613648b26712a1cc16
shogo4131 <shougo08697118@gmail.com>
2023-01-19 23:21:59 +0900

    feat: add layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,16 @@
+import type { FC, ReactNode } from 'react';
+
+import { Header } from '../Header';
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Layout: FC<Props> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+};
