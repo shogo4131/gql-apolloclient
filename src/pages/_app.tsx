6235d7acad79de07b87b64a65ac701fdcafadcd7
shogo4131 <shougo08697118@gmail.com>
2023-02-01 21:36:33 +0900

    feat: add apollo client setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache,
+});
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider resetCSS>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider resetCSS>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
   );
 };
 
