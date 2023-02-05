536c215777b780328f7d3a7d0f66f9d561660cb5
shogo4131 <shougo08697118@gmail.com>
2023-02-05 14:24:02 +0900

    fix: user lists ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import { Fragment } from 'react';
-
 import type { NextPage } from 'next';
+
+import { Heading, HStack, Text } from '@chakra-ui/react';
 
 import { Layout } from '../components/Layout';
 import { useUsersQuery } from '../graphql/generated/graphql';
@@ -12,12 +12,14 @@ const Home: NextPage = () => {
 
   return (
     <Layout>
-      {data?.users.map((user) => (
-        <Fragment key={user.id}>
-          <div>{user.id}</div>
-          <div>{user.name}</div>
-          <div>{user.email}</div>
-        </Fragment>
+      <Heading as="h1" mb={10}>
+        ユーザー一覧
+      </Heading>
+      {data?.users.map(({ id, name }) => (
+        <HStack key={id} spacing={8} fontSize={20}>
+          <Text>{id}</Text>
+          <Text>{name}</Text>
+        </HStack>
       ))}
     </Layout>
   );
