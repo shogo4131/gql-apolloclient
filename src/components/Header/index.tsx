7f1b455d1d25fb94ee5654347c89878db0b0115c
shogo4131 <shougo08697118@gmail.com>
2023-02-05 14:23:36 +0900

    fix: change header link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,14 +8,14 @@ export const Header: FC = () => {
   return (
     <HStack justifyContent="space-between" height={70} padding={6} bg="gray.100">
       <Box>
-        <Link href="/">GraphQL勉強会 サンプル</Link>
+        <Link href="/">本の貸出アプリ</Link>
       </Box>
       <Box display="flex">
         <Box marginRight={4}>
-          <Link href="signin">ログイン</Link>
+          <Link href="/">ユーザー一覧</Link>
         </Box>
         <Box>
-          <Link href="signup">新規登録</Link>
+          <Link href="/register-user">新規ユーザー登録</Link>
         </Box>
       </Box>
     </HStack>
