311f645532cfe7bc40a870ec017a6049485cdb03
shogo4131 <shougo08697118@gmail.com>
2023-02-05 15:03:51 +0900

    feat: add button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,15 @@
 import type { FC, ReactNode } from 'react';
 
+import { Button as ChakraButton, type ButtonProps } from '@chakra-ui/react';
+
 type Props = {
   children: ReactNode;
-};
+} & ButtonProps;
 
-export const Button: FC<Props> = ({ children }) => {
-  return <button>{children}</button>;
+export const Button: FC<Props> = ({ children, ...rest }) => {
+  return (
+    <ChakraButton type="button" {...rest}>
+      {children}
+    </ChakraButton>
+  );
 };
