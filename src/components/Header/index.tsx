805e830db7afb43052b786030c3ff7d7393eb91f
shogo4131 <shougo08697118@gmail.com>
2023-02-06 18:51:19 +0900

    feat: add rental books page link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,13 +6,16 @@ import { HStack, Box } from '@chakra-ui/react';
 
 export const Header: FC = () => {
   return (
-    <HStack justifyContent="space-between" height={70} padding={6} bg="gray.100">
+    <HStack as="nav" justifyContent="space-between" height={70} padding={6} bg="gray.100">
       <Box>
         <Link href="/">本の貸出アプリ</Link>
       </Box>
       <Box display="flex">
         <Box marginRight={4}>
           <Link href="/">ユーザー一覧</Link>
+        </Box>
+        <Box marginRight={4}>
+          <Link href="/rental-books">貸出書一覧</Link>
         </Box>
         <Box>
           <Link href="/create-user">新規ユーザー登録</Link>
