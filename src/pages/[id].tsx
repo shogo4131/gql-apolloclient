664de75ebb791495131abcc5377482e21f3e72b4
shogo4131 <shougo08697118@gmail.com>
2023-02-06 19:48:08 +0900

    feat: add remove user func

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,14 +1,56 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
-import { useUserQuery } from '../graphql/generated/graphql';
+import { Table, Thead, Tbody, Tr, Th, Td, TableContainer, Heading, Box } from '@chakra-ui/react';
+
+import { Button } from '../components/Button';
+import { Layout } from '../components/Layout';
+import { useUserQuery, useRemoveUserMutation } from '../graphql/generated/graphql';
 
 const UserDetail: NextPage = () => {
-  const { query } = useRouter();
+  const { query, push } = useRouter();
 
   const { data } = useUserQuery({ variables: { id: query.id as string } });
+  const [deleteUser] = useRemoveUserMutation({
+    variables: { id: query.id as string },
+  });
 
-  return <div>{data?.user.name}</div>;
+  const onClickRemoveUserHandler = () => {
+    if (confirm('本当に削除しますか？')) {
+      deleteUser();
+      // TODO: 遷移した際にキャッシュが残っている
+      push('/');
+    }
+  };
+
+  return (
+    <Layout>
+      <TableContainer>
+        <Heading as="h1" mb={10}>
+          ユーザー詳細情報
+        </Heading>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>名前</Th>
+              <Th>メールアドレス</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>{data?.user.name}</Td>
+              <Td>{data?.user.email}</Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <Box display="flex" justifyContent="end" mt={10}>
+        <Button type="button" onClick={onClickRemoveUserHandler}>
+          削除
+        </Button>
+      </Box>
+    </Layout>
+  );
 };
 
 export default UserDetail;
